3d6a51dd415258fb8ffe13e481f983b9bf1af889
bishtjaswant <jaswantbishtuk1995@gmail.com>
2020-05-19 13:33:32 +0530

    twitter  like  website

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily:{
+        'roboto':['Roboto','sans-serif'],
+        'work-sans':['Work Sans','sans-serif']
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
